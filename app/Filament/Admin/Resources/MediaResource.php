d7657b8c6b7c67e1108a8c04868d8372d92f554d
Jean Giordani <jeangiordani290@gmail.com>
2024-06-07 17:36:47 -0300

    feat: add media upload

---
+++ app/Filament/Admin/Resources/MediaResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\MediaResource\Pages;
+use App\Models\Media;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class MediaResource extends Resource
+{
+    protected static ?string $model = Media::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\FileUpload::make('path')
+                    ->directory('uploads')
+                    ->disk('public')
+                    ->unique($table = 'medias', $column = 'path'),
+
+                Forms\Components\Select::make('type')
+                ->options([
+                    'image' => 'Imagem',
+                    'video' => 'Video',
+                    'audio' => 'Audio',
+                    'PDF' => 'PDF'
+                ])
+                ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('path')->url(fn (Media $record) => asset('storage/uploads/' . $record->path)),
+                TextColumn::make('type'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMedia::route('/'),
+            'create' => Pages\CreateMedia::route('/create'),
+            'edit' => Pages\EditMedia::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Media.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Media extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'name',
+        'path',
+        'type'
+    ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $casts = [
+        'id' => 'string',
+    ];
+
+    protected $table = 'medias';
+
+    // protected static function boot()
+    // {
+    //     parent::boot();
+
+    //     static::creating(function ($model) {
+    //         $model->{$model->getKeyName()} = (string) Str::uuid();
+    //     });
+
+    //     static::saving(function ($model) {
+    //         if ($model->isDirty('path') && !empty($model->path)) {
+    //             $extension = pathinfo($model->path, PATHINFO_EXTENSION);
+    //             $filename = $model->id . '.' . $extension;
+    //             $model->path = 'uploads/' . $filename;
+    //         }
+    //     });
+    // }
+}
